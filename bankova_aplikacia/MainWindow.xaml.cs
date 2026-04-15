a2eda36b866c16c5f02518464ebb1cdcfed6448a
Mark <arendarikmar@gmail.com>
2026-04-15 09:36:25 +0200

    buttons change 1

--- bankova_aplikacia/MainWindow.xaml.cs
+++ bankova_aplikacia/MainWindow.xaml.cs
@@ -20,5 +20,73 @@ namespace bankova_aplikacia
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string nazov = NazovV1.Text;
+            string sumaText = Suma1.Text;
+
+            double suma;
+
+            if (string.IsNullOrWhiteSpace(nazov))
+                return;
+
+            if (!double.TryParse(sumaText, out suma))
+            {
+                MessageBox.Show("Zadaj platnú číselnú hodnotu!");
+                return;
+            }
+
+            ZoznamVydavkov.Items.Add($"{nazov} - {suma} €");
+
+            NazovV1.Clear();
+            Suma1.Clear();
+
+            if (string.IsNullOrWhiteSpace(NazovV1.Text))
+            {
+                NazovV1.Text = "Názov výdavku";
+                NazovV1.Foreground = Brushes.Gray;
+            }
+            if (string.IsNullOrWhiteSpace(Suma1.Text))
+            {
+                Suma1.Text = "0";
+                Suma1.Foreground = Brushes.Gray;
+            }
+        }
+        private void NazovBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (NazovV1.Text == "Názov výdavku")
+            {
+                NazovV1.Text = "";
+                NazovV1.Foreground = Brushes.Black;
+            }
+        }
+
+        private void NazovBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(NazovV1.Text))
+            {
+                NazovV1.Text = "Názov výdavku";
+                NazovV1.Foreground = Brushes.Gray;
+            }
+        }
+
+        private void SumaBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (Suma1.Text == "0")
+            {
+                Suma1.Text = "";
+                Suma1.Foreground = Brushes.Black;
+            }
+        }
+
+        private void SumaBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(Suma1.Text))
+            {
+                Suma1.Text = "0";
+                Suma1.Foreground = Brushes.Gray;
+            }
+        }
     }
 }
